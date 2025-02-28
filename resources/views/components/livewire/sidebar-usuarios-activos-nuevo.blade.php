0c3a875caa0af5884d78c6b9473d5ba1a8bd9446
Adolfo <adolfodjhz@gmail.com>
2025-02-28 13:12:15 -0600

    Update siderbar

--- resources/views/components/livewire/sidebar-usuarios-activos-nuevo.blade.php
+++ resources/views/components/livewire/sidebar-usuarios-activos-nuevo.blade.php
@@ -1,18 +1,20 @@
 <div class="min-w-fit">
     <!-- Sidebar backdrop (mobile only) -->
     <div class="fixed inset-0 bg-gray-900 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200"
-        :class="sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'" aria-hidden="true" x-cloak></div>
+        :class="sidebarOpenActive ? 'opacity-100' : 'opacity-0 pointer-events-none'" aria-hidden="true" x-cloak></div>
 
     <!-- Sidebar -->
-    <div id="sidebar"
+    <div id="sidebarActive"
         class="flex lg:!flex flex-col absolute z-40 right-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out transform lg:transform-none"
-        :class="sidebarOpen ? 'translate-x-0' : 'translate-x-full'" @keydown.escape.window="sidebarOpen = false">
+        :class="sidebarOpenActive ? 'translate-x-0' : 'translate-x-full'"
+        @keydown.escape.window="sidebarOpenActive = false">
 
         <!-- Sidebar header -->
         <div class="flex justify-between mb-10 pr-3 sm:px-2">
             <!-- Close button -->
-            <button class="lg:hidden text-gray-500 hover:text-gray-400" @click.stop="sidebarOpen = !sidebarOpen"
-                aria-controls="sidebar" :aria-expanded="sidebarOpen">
+            <button class="lg:hidden text-gray-500 hover:text-gray-400"
+                @click.stop="sidebarOpenActive = !sidebarOpenActive" aria-controls="sidebar"
+                :aria-expanded="sidebarOpenActive">
                 <span class="sr-only">Close sidebar</span>
                 <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
