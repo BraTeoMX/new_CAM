744a46bd005bb4651aa4c9ecd270b9e2f511e892
ADJHz <adolfodjhz@gmail.com>
2025-06-03 18:12:10 -0600

    Sidebar

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -34,7 +34,23 @@
                     <img src="{{ asset('images/intimark.webp') }}" alt="Logo" class="h-12 w-auto mx-auto" />
                 </a>
             </div>
-
+                 <!-- Expand/collapse button (always top, only on lg+) -->
+                <div class="hidden lg:block">
+                    <button
+                        class="p-2 rounded-full text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 transition-colors"
+                        @click="
+                            sidebarExpanded = !sidebarExpanded;
+                            sidebarOpen = false;
+                        ">
+                        <span class="sr-only">Expandir/Colapsar sidebar</span>
+                        <svg class="shrink-0 fill-current text-gray-400 dark:text-gray-500 sidebar-expanded:rotate-180"
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M15 16a1 1 0 0 1-1-1V1a1 1 0 1 1 2 0v14a1 1 0 0 1-1 1ZM8.586 7H1a1 1 0 1 0 0 2h7.586l-2.793 2.793a1 1 0 1 0 1.414 1.414l4.5-4.5A.997.997 0 0 0 12 8.01M11.924 7.617a.997.997 0 0 0-.217-.324l-4.5-4.5a1 1 0 0 0-1.414 1.414L8.586 7M12 7.99a.996.996 0 0 0-.076-.373Z" />
+                        </svg>
+                    </button>
+                </div>
             <!-- Navigation -->
             <nav class="flex-1 flex flex-col justify-between">
                 <div>
@@ -155,21 +171,6 @@
                             </div>
                         </li>
                     </ul>
-                </div>
-
-                <!-- Expand / collapse button -->
-                <div class="hidden lg:flex flex-col justify-end">
-                    <button
-                        class="mx-auto mb-2 p-2 rounded-full text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400 transition-colors"
-                        @click="sidebarExpanded = !sidebarExpanded">
-                        <span class="sr-only">Expand / collapse sidebar</span>
-                        <svg class="shrink-0 fill-current text-gray-400 dark:text-gray-500 sidebar-expanded:rotate-180"
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 16 16">
-                            <path
-                                d="M15 16a1 1 0 0 1-1-1V1a1 1 0 1 1 2 0v14a1 1 0 0 1-1 1ZM8.586 7H1a1 1 0 1 0 0 2h7.586l-2.793 2.793a1 1 0 1 0 1.414 1.414l4.5-4.5A.997.997 0 0 0 12 8.01M11.924 7.617a.997.997 0 0 0-.217-.324l-4.5-4.5a1 1 0 0 0-1.414 1.414L8.586 7M12 7.99a.996.996 0 0 0-.076-.373Z" />
-                        </svg>
-                    </button>
                 </div>
             </nav>
         </aside>
