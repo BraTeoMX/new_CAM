d975991d7894bff6c9158bc166541fe3f575e407
Brayam Programador-BraTeoMX <brateomx@gmail.com>
2025-07-09 16:36:56 -0600

    quitar crf de mas

--- resources/views/vinculacion/index.blade.php
+++ resources/views/vinculacion/index.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
         <!-- Título -->
         <div class="sm:flex sm:justify-between sm:items-center mb-8">
