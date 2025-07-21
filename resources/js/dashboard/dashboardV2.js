c840a35f01c12e8a578d1ff1bfc4b819260bab5d
Brayam Programador-BraTeoMX <brateomx@gmail.com>
2025-07-21 12:40:18 -0600

    creacion del archivo de la primera seccion que calcula los minutos

--- resources/js/dashboard/dashboardV2.js
+++ resources/js/dashboard/dashboardV2.js
@@ -1,9 +1,10 @@
 // resources/js/dashboard.js
 import './calendarSelects.js';
+import './calculoMinutos.js';
 import './elemento1dashboard.js';
 import './dashboardHeatmap.js';
 import './linetime.js';
 import './efectividad.js';
 import './tops.js';
 import './creatComplet.js';
-import './minmachdesc.js';
+
