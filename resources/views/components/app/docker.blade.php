f1c50762528f93fca35503b76b31a325c9337a51
Brayam Programador-BraTeoMX <brateomx@gmail.com>
2025-09-08 11:17:27 -0600

    ajuste de ruta en docker

--- resources/views/components/app/docker.blade.php
+++ resources/views/components/app/docker.blade.php
@@ -88,9 +88,9 @@
                         <span class="material-symbols-outlined w-5 h-5 mr-2">admin_panel_settings</span>
                         Administrar Usuarios
                     </a>
-                    <a href="{{ route('vinculacion') }}"
+                    <a href="{{ route('vinculacion.index') }}"
                         class="flex items-center px-4 py-2 text-xs text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600
-                        @if (Request::segment(1) === 'vinculacion.index') bg-[linear-gradient(135deg,var(--tw-gradient-stops))] from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04] @endif">
+                        @if (Request::segment(1) === 'vinculacion') bg-[linear-gradient(135deg,var(--tw-gradient-stops))] from-violet-500/[0.12] dark:from-violet-500/[0.24] to-violet-500/[0.04] @endif">
                         <span class="material-symbols-outlined w-5 h-5 mr-2">library_books</span>
                         Catálogos
                     </a>
